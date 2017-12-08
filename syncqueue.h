23f8a39c5cfdd45a5bd931784c774ca893d46d1c
wangmin <wangminsuccess@gmail.com>
2017-12-08 23:17:38 +0800

    hello

--- syncqueue.h
+++ syncqueue.h
@@ -2,6 +2,8 @@
 #include<mutex>
 #include<iostream> 
 #include<utility> //forward
+#include<condition_variable>
+
 template<typename T>
 class SyncQueue{
 public:
@@ -11,13 +13,25 @@ public:
 		Add(x);
 	}
 	void Put(T&& x){
-		Add(std::forward<T>x);
+		Add(std::forward<T>(x));
 	}
 	void Take(std::list<T>& list){
-		
+		std::unique_lock<mutex> locker(m_mutex);
+		m_notEmpty.wait(locker,[this]{ return m_needStop||NotEmpty();});
+		//while (!pred()) wait(locker);
+		if(m_needStop)
+			return;
+		list=std::move(m_queue);
+		m_notFull.notify_one();
 	}
 	void Take(T& t){
-		
+		std::unique_lock<mutex> locker(m_mutex);
+		m_notEmpty.wait(locker,[this]{return m_needStop||NotEmpty();});
+		if(m_needStop)
+			return;
+		t=m_queue.front();
+		m_queue.pop_front();
+		m_notFull.notify_one();
 	}
 	void Stop(){
 		
@@ -30,6 +44,14 @@ public:
 	size_t Size(){
 	}
 private:
+	bool NotEmpty()const{
+		
+	}
+private:
+	std::list<T> m_queue;
+	std::mutex m_mutex;
+	std::condition_variable m_notEmpty;//不空的信号
+	std::condition_variable m_notFull;//不满的信号
 	int m_maxSize;
 	bool m_needStop;	
 };
